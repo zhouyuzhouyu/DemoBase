ca60f88d8a4ac5d788fa98d1c743449a0a87b37f
zhouyu <yzhou@niub.la>
2015-08-11 11:45:30 +0800

    init

---
+++ BlogCollect/BlogCollect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogCollect
+//
+//  Created by 周玉 on 15/8/11.
+//  Copyright (c) 2015年 周玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
